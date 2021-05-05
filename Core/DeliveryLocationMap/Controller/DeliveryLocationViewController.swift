201f4271e1f08d13b657cc3399b3c5255a9b12fe
Chingiz <k.chyngiz@gmail.com>
2021-05-05 18:03:48 +0600

    added delivery location. check

--- Core/DeliveryLocationMap/Controller/DeliveryLocationViewController.swift
+++ Core/DeliveryLocationMap/Controller/DeliveryLocationViewController.swift
@@ -1,6 +1,7 @@
 import RxSwift
 import CoreLocation
 import YandexMapsMobile
+import MapKit
 import UIKit
 
 class DeliveryLocationViewController: UIViewController, DeliveryLocationModule, ViewHolder {
@@ -10,7 +11,20 @@ class DeliveryLocationViewController: UIViewController, DeliveryLocationModule, 
     private let viewModel: DeliveryLocationMapViewModel
     private let disposeBag = DisposeBag()
     private let userLocationStatusSubject: PublishSubject<UserLocationStatus> = .init()
-
+    let points = [
+        YMKPoint(latitude: 42.271008, longitude: 69.558747),
+        YMKPoint(latitude: 42.382227, longitude: 69.491084),
+        YMKPoint(latitude: 42.410608, longitude: 69.636103),
+        YMKPoint(latitude: 42.311006, longitude: 69.660448)
+    ]
+    
+    let lines = [
+        CLLocationCoordinate2D(latitude: 42.271008, longitude: 69.558747),
+        CLLocationCoordinate2D(latitude: 42.382227, longitude: 69.491084),
+        CLLocationCoordinate2D(latitude: 42.410608, longitude: 69.636103),
+        CLLocationCoordinate2D(latitude: 42.311006, longitude: 69.660448)
+    ]
+    
     private let locationManager = CLLocationManager()
     override func loadView() {
         view = DeliveryLocationMapView()
@@ -27,11 +41,13 @@ class DeliveryLocationViewController: UIViewController, DeliveryLocationModule, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
         viewModel.mapManager.setupCameraListener(in: rootView.mapView)
         bindViewModel()
         setupMap()
+        deliveryZone()
     }
     
     private func bindViewModel() {
@@ -49,12 +65,19 @@ class DeliveryLocationViewController: UIViewController, DeliveryLocationModule, 
             self.rootView.textField.text = location.name
             self.deliveryLocationObject.onNext(location)
         }).disposed(by: disposeBag)
+        let deliveryPolygon = MKPolygon(coordinates: lines, count: 4)
         
         rootView.saveButton.rx.tap
             .withLatestFrom(deliveryLocationObject)
             .subscribe(onNext: { [unowned self] location in
-                self.saveLocationToCache(location: location)
-                self.onlocationDidSelect?(location)
+                let deliveryCoordinate = CLLocationCoordinate2D(latitude: location.point.latitude, longitude: location.point.longitude)
+                let isOnRegion = deliveryPolygon.contain(coor: deliveryCoordinate)
+                if isOnRegion {
+                    self.saveLocationToCache(location: location)
+                    self.onlocationDidSelect?(location)
+                } else {
+                    self.showErrorInAlert(text: "К сожалению, доставка работает только в черте города Шымкент")
+                }
             })
             .disposed(by: disposeBag)
         
@@ -92,6 +115,27 @@ class DeliveryLocationViewController: UIViewController, DeliveryLocationModule, 
     private func saveLocationToCache(location: DeliveryLocation) {
         viewModel.saveAdress(adress: location)
     }
+    
+    
+    private func deliveryZone() {
+
+        let addPolygon = YMKPolygon(outerRing: YMKLinearRing(points: points), innerRings: [])
+        
+        let mapObjects = rootView.mapView.mapWindow.map.mapObjects.addPolygon(with: addPolygon)
+        
+        mapObjects.strokeColor = .primary
+        mapObjects.strokeWidth = 3
+        mapObjects.fillColor = UIColor.primary.withAlphaComponent(0.1)
+    }
+    
+    func getLocationSucces(deliveryLocation: YMKPoint) -> Bool {
+        if let userCoordinate = locationManager.location?.coordinate {
+            let distance = YMKDistance(YMKPoint(latitude: userCoordinate.latitude, longitude: userCoordinate.longitude), deliveryLocation)
+            return distance < 11000
+        } else {
+            return false
+        }
+    }
 }
 
 extension DeliveryLocationViewController: CLLocationManagerDelegate {
@@ -101,5 +145,44 @@ extension DeliveryLocationViewController: CLLocationManagerDelegate {
             return
         }
         userLocationStatusSubject.onNext(.enabled(lat: coordinates.latitude, long: coordinates.longitude))
+    }
+}
+
+extension CLLocationCoordinate2D {
+
+    func liesInsideRegion(region: [CLLocationCoordinate2D]) -> Bool {
+        var liesInside = false
+        var i = 0
+        var j = region.count-1
+
+        while i < region.count {
+
+            let iCoordinate = region[i]
+            let jCoordinate = region[j]
+
+            if (iCoordinate.latitude > self.latitude) != (jCoordinate.latitude > self.latitude) {
+                if self.longitude < (iCoordinate.longitude - jCoordinate.longitude) * (self.latitude - iCoordinate.latitude) / (jCoordinate.latitude-iCoordinate.latitude) + iCoordinate.longitude {
+                    liesInside = !liesInside
+                    }
+            }
+
+            i += 1
+            j = i+1
+        }
+        return liesInside
+    }
+
+}
+
+extension MKPolygon {
+    func contain(coor: CLLocationCoordinate2D) -> Bool {
+        let polygonRenderer = MKPolygonRenderer(polygon: self)
+        let currentMapPoint: MKMapPoint = MKMapPoint(coor)
+        let polygonViewPoint: CGPoint = polygonRenderer.point(for: currentMapPoint)
+        if polygonRenderer.path == nil {
+          return false
+        }else{
+          return polygonRenderer.path.contains(polygonViewPoint)
+        }
     }
 }
